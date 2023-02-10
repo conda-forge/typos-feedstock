49821ed94e1128b2fb823949d6d53420beff34cb
Jan Tilly <jan.tilly@quantco.com>
2023-02-10 08:16:37 +0100

    Use m2w64_c and strip debug symbols.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,9 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 :: build
 cargo install --locked --root "%PREFIX%" --path . || goto :error
 
+:: strip debug symbols
+strip "%LIBRARY_PREFIX%\bin\typos.exe" || goto :error
+
 :: remove extra build file
 del /F /Q "%PREFIX%\.crates.toml"
 
